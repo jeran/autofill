ca083d77a3301fd220d917226a5929640be75a49
jeran <jerantfox@gmail.com>
2023-03-25 18:15:17 -0400

    Remove revers dns naming from package structure

--- app/src/main/kotlin/autofill/MainActivity.kt
+++ app/src/main/kotlin/autofill/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.jeran.autofill
+package autofill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.jeran.autofill.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
